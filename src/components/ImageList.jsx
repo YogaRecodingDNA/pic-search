d3eabfe9e52619b903bb61fda0aa4b9aa73f5c21
Chadd Ho <74035551+YogaRecodingDNA@users.noreply.github.com>
2023-01-08 07:47:46 -0800

    IMAGE LIST: Map through images and render thumbnails

--- src/components/ImageList.jsx
+++ src/components/ImageList.jsx
@@ -2,10 +2,11 @@ import ImageShow from "./ImageShow";
 
 function ImageList({ allImages }) {
     
+  const renderThumbs = allImages.map((image, index) => <img src={image.urls.thumb} alt={image.alt_description} />);
 
   return (
     <div>
-      ImageList
+       {renderThumbs}
     </div>
   )
 };
